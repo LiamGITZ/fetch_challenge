7f127ab16aa2e9c56daa64b0c240d176cd530294
liamgitz <lianwalsh@gmail.com>
2021-02-23 16:45:06 -0800

    added transaction checking and insertion into sorted list

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
+from collections import defaultdict
+import types
 
 transactions = []
-payers = {}
+payers = defaultdict(int)
 
+class transaction:
+    def __init__(self, payer, points, timestamp):
+        self.payer = payer
+        self.points = points
+        self.timestamp = timestamp
+
+    def __lt__(self, transaction2):
+        return transaction2.timestamp > self.timestamp
+
+    def __eq__(self, transaction2):
+        return     self.payer     == transaction2.payer \
+               and self.points    == transaction2.points \
+               and self.timestamp == transaction2.timestamp
 
 from flask import Flask, request
 from datetime import datetime
@@ -21,18 +36,20 @@ def json_example():
     print(request_data)
     if not request_data:
         return('invalid no data', 400)
-    elif not request_data['payer'] or request_data['payer'] == '':
+    elif not 'payer' in request_data or request_data['payer'] == '':
         return('no payer information')
-    elif not request_data['points']:
+    elif not 'points' in request_data:
         return('no point value', 400)
-    elif not request_data['timestamp'] or request_data['timestamp'] <= 0:
+    elif not 'timestamp' in request_data or request_data['timestamp'] <= 0:
         return('invalid date', 400)
-    elif (request_data['timestamp'], request_data) in transactions:
-        return('transaction already recorded', 400)
     else:
-        bisect.insort_right(transactions, (request_data['timestamp'], request_data))
+        given_transaction = transaction(request_data['payer'], request_data['points'], request_data['timestamp'])
+        if given_transaction in transactions:
+            return('transaction already recorded', 400)
+        bisect.insort_right(transactions, given_transaction)
+        payers[given_transaction.payer] += given_transaction.points
 
-    date = datetime.utcfromtimestamp(request_data['timestamp'])
+    date = datetime.utcfromtimestamp(given_transaction.timestamp)
     date_time = date.strftime("%m/%d/%Y, %H:%M:%S")
     return('Recieved Transaction for time: ' + str(date_time))
 
